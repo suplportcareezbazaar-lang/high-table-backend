ac954e1302a0bfd7b4431cdeb874a4a3f1b51a6d
suplportcareezbazaar-lang <suplportcareezbazaar@gmail.com>
2026-02-14 21:23:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -54,13 +54,9 @@ const allowedOrigins = process.env.NODE_ENV === "production"
     ];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: [
+        "https://high-table-frontend.onrender.com"
+    ],
     credentials: true
 }));
 
