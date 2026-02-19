f9cc81621dc5cbeb0d62520e77512771ddee46a8
suplportcareezbazaar-lang <suplportcareezbazaar@gmail.com>
2026-02-19 15:16:55 +0530

    Update index.js

--- services/index.js
+++ services/index.js
@@ -1,9 +1,23 @@
 const { getCricketMatches } = require("./cricketApi");
+const { getFootballMatches } = require("./footballApi");
+const { getBasketballMatches } = require("./basketballApi");
+const { getMmaMatches } = require("./mmaApi");
 
+/* ================= MATCHES ================= */
 async function getAllMatches() {
     try {
-        const cricket = await getCricketMatches();
-        return cricket;
+        const results = await Promise.all([
+            getCricketMatches(),
+            getFootballMatches(),
+            getBasketballMatches(),
+            getMmaMatches()
+        ]);
+
+        const all = results.flat().filter(Boolean);
+
+        console.log("Total matches fetched:", all.length);
+
+        return all;
     } catch (err) {
         console.error("getAllMatches error:", err.message);
         return [];
